f80dc2e87bd07978d8afb6f1a731300938e9e95d
Zhipeng Xie <xiezhipeng1@huawei.com>
2020-03-27 06:27:22 -0400

    upgrade the license from Mulan PSL v1 to v2
    Signed-off-by: Zhipeng Xie <xiezhipeng1@huawei.com>

--- scripts/swap/set.sh
+++ scripts/swap/set.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Copyright (c) 2019 Huawei Technologies Co., Ltd.
-# A-Tune is licensed under the Mulan PSL v1.
-# You can use this software according to the terms and conditions of the Mulan PSL v1.
-# You may obtain a copy of Mulan PSL v1 at:
+# A-Tune is licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
 #     http://license.coscl.org.cn/MulanPSL
 # THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
 # PURPOSE.
-# See the Mulan PSL v1 for more details.
+# See the Mulan PSL v2 for more details.
 # Create: 2019-10-29
 
 command -v swapoff >/dev/null 2>&1
