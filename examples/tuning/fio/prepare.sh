d9a0352334b95b8c9416af0145151310187eced1
chenziyang <chenziyang4@huawei.com>
2022-02-10 21:11:04 +0800

    Fix prepare.sh regular expression bug, and add '-' to server.yaml

--- examples/tuning/fio/prepare.sh
+++ examples/tuning/fio/prepare.sh
@@ -8,9 +8,9 @@ cur=$(
 )
 
 echo "update the test script、client and server yaml files"
-sed -i "s#^FILENAME.*#^FILENAME=${disk}#g" $cur/fio-test.sh
-sed -i "s# .*fio/fio-test.sh# $cur/fio-test.sh#g" $cur/tuning_fio_server.yaml
-sed -i "s#' .*fio/fio-test.sh#' $cur/fio-test.sh#g" $cur/tuning_fio_client.yaml
+sed -i "s#^FILENAME.*#FILENAME=${disk}#g" $cur/fio-test.sh
+sed -i "s# fio/fio-test.sh# $cur/fio-test.sh#g" $cur/tuning_fio_server.yaml
+sed -i "s# fio/fio-test.sh# $cur/fio-test.sh#g" $cur/tuning_fio_client.yaml
 
 echo "copy the server yaml file to /etc/atuned/tuning/"
 cp $cur/tuning_fio_server.yaml /etc/atuned/tuning/
