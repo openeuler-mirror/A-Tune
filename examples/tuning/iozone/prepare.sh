e48cee96abf3117ea5e0a6aa841bfa4de47fec2f
mmlidc <mmlidc@isoftstone.com>
2023-02-15 18:10:35 +0800

    修改离线调优示例iozone中prepare.sh文件中的错误

--- examples/tuning/iozone/prepare.sh
+++ examples/tuning/iozone/prepare.sh
@@ -15,4 +15,4 @@ sed -i "s# .*iozone/iozone-test.sh# $cur/iozone-test.sh#g" $cur/tuning_iozone_se
 sed -i "s#DISKNAME#'${disk}#g" $cur/tuning_iozone_client.yaml
 
 echo "copy the server yaml file to /etc/atuned/tuning/"
-cp $cur/tuning_fio_server.yaml /etc/atuned/tuning/
+cp $cur/tuning_iozone_server.yaml /etc/atuned/tuning/
