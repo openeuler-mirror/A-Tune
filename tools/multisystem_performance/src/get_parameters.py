465f4a9479156c7ebf7a34df6ff7e104458bc31c
westtide <tocokeo@outlook.com>
2023-09-22 13:44:32 +0800

    update1

--- tools/multisystem_performance/src/get_parameters.py
+++ tools/multisystem_performance/src/get_parameters.py
@@ -1,19 +1,5 @@
-import subprocess
-import re
 import difflib
-import random
-import string
-from datetime import datetime
-import logging
-
-#生成时间戳，用于文件命名
-def generate_timestamp_string():
-    now = datetime.now()
-    timestamp_string = now.strftime("%Y-%m-%d-%H-%M-%S")
-    return timestamp_string
-
-timestamp = generate_timestamp_string()
-
+from load_check import *
 
 log_file = f'./log/get_sysctl_ulimit_{timestamp}.log'
 file_handler = logging.FileHandler(log_file)
@@ -23,7 +9,7 @@ logging.getLogger().setLevel(logging.INFO)
 logging.info('This is a log message')
 
 
-#获取 linux 版本，CentOS Steam 或者 OpenEuler
+# 获取 linux 版本，CentOS Steam 或者 OpenEuler
 def get_os_version():
     linux_version = "Unknown"
     try:
@@ -36,10 +22,11 @@ def get_os_version():
     except FileNotFoundError:
         logging.error('failed: \'cat /etc/os-release\' ', extra={'logfile': log_file})
         print("未找到 /etc/os-release 文件，请检查文件是否存在。")
-    
+
     return linux_version
 
-#bash: "syscyl -a" 存储在sysctl@$linux-version$中
+
+# bash: "syscyl -a" 存储在sysctl@$linux-version$中
 def run_sysctl_command_and_save_result(file_name):
     bash_command = "sysctl -a"
     with open(file_name, "w") as output_file:
@@ -50,7 +37,8 @@ def run_sysctl_command_and_save_result(file_name):
             print(f"命令执行出错：{stderr.decode()}")
             logging.error('failed: run command \'%s\' ', bash_command, extra={'logfile': log_file})
 
-#bash: "ulimit -a" 存储在ulimit@$linux-version$中
+
+# bash: "ulimit -a" 存储在ulimit@$linux-version$中
 def run_ulimit_command_and_save_result(file_name):
     bash_command = "ulimit -a"
     with open(file_name, "w") as output_file:
@@ -61,17 +49,21 @@ def run_ulimit_command_and_save_result(file_name):
             print(f"命令执行出错：{stderr.decode()}")
             logging.error('failed: run command \'%s\' ', bash_command, extra={'logfile': log_file})
 
+
 # 执行sysctl -a与ulimit -a命令并保存结果
-def run_command_save(sysctl_res_file_name,ulimit_res_file_name):
+def run_command_save(sysctl_res_file_name, ulimit_res_file_name):
     # 执行sysctl -a命令并保存结果到sysctl@xxx.txt文件中
-    run_sysctl_command_and_save_result("./data/"+sysctl_res_file_name)
-    logging.info('run_sysctl_command_and_save_result successfully, save in \'%s\'',"./data/"+sysctl_res_file_name, extra={'logfile': log_file})
+    run_sysctl_command_and_save_result("./data/" + sysctl_res_file_name)
+    logging.info('run_sysctl_command_and_save_result successfully, save in \'%s\'', "./data/" + sysctl_res_file_name,
+                 extra={'logfile': log_file})
     # 执行ulimit -a命令并保存结果到ulimit@xxx.txt文件中
-    run_ulimit_command_and_save_result("./data/"+ulimit_res_file_name)
-    logging.info('run_ulimit_command_and_save_result successfully, save in \'%s\'',"./data/"+ulimit_res_file_name, extra={'logfile': log_file})
+    run_ulimit_command_and_save_result("./data/" + ulimit_res_file_name)
+    logging.info('run_ulimit_command_and_save_result successfully, save in \'%s\'', "./data/" + ulimit_res_file_name,
+                 extra={'logfile': log_file})
+
 
 # 使用difflib的比较，保存比较结果diff_result到文件save_difflib_res
-def use_differ_res(os_version,sysctl_res_file_name,save_difflib_res):
+def use_differ_res(os_version, sysctl_res_file_name, save_difflib_res):
     # 根据当前操作系统匹配对方的sysctl@xxx文件，使用scp获取对方配置
     file1 = sysctl_res_file_name
     if os_version == "openEuler":
@@ -87,32 +79,36 @@ def use_differ_res(os_version,sysctl_res_file_name,save_difflib_res):
             subprocess.run(command, shell=True, check=True)
         except subprocess.CalledProcessError as e:
             print(f"执行命令出错：{e}")
-        file2 = "sysctl@openEuler.txt"    
+        file2 = "sysctl@openEuler.txt"
 
-   # 使用difflib的比较file1与file2的结果
-    with open("./data/"+file1) as f1, open("./data/"+file2) as f2:
-        logging.info('compare file1 = \'%s\', file2 = \'%s\'',"./data/"+file1,"./data/"+file2, extra={'logfile': log_file})
+        # 使用difflib的比较file1与file2的结果
+    with open("./data/" + file1) as f1, open("./data/" + file2) as f2:
+        logging.info('compare file1 = \'%s\', file2 = \'%s\'', "./data/" + file1, "./data/" + file2,
+                     extra={'logfile': log_file})
         text1 = f1.readlines()
         text2 = f2.readlines()
-        logging.info('text1 in \'%s\', text2 in \'%s\'',"./data/"+file1, "./data/"+file2,extra={'logfile': log_file})
-    diff=difflib.Differ()
+        logging.info('text1 in \'%s\', text2 in \'%s\'', "./data/" + file1, "./data/" + file2,
+                     extra={'logfile': log_file})
+    diff = difflib.Differ()
     diff_result = diff.compare(text1, text2)
-    logging.info('compare file1 and file2 by difflib successfully', extra={'logfile': log_file})   
+    logging.info('compare file1 and file2 by difflib successfully', extra={'logfile': log_file})
 
-    #保存比较结果diff_result到文件save_difflib_res
+    # 保存比较结果diff_result到文件save_difflib_res
     try:
-        with open("./data/"+save_difflib_res, 'w') as file:
+        with open("./data/" + save_difflib_res, 'w') as file:
             file.write("".join(diff_result))
-        logging.info('save diff_result to file \'%s\' successfully', "./data/"+save_difflib_res, extra={'logfile': log_file})
+        logging.info('save diff_result to file \'%s\' successfully', "./data/" + save_difflib_res,
+                     extra={'logfile': log_file})
         print(f"比较结果已成功保存到文件: ./data/{save_difflib_res}")
     except IOError:
-        logging.error('failed: save diff_result to file \'%s\' ', "./data/"+save_difflib_res, extra={'logfile': log_file})
+        logging.error('failed: save diff_result to file \'%s\' ', "./data/" + save_difflib_res,
+                      extra={'logfile': log_file})
         print("保存文件时出现错误，请检查路径和文件权限。")
 
 
-
 # 显式分析保存比较结果到文件
-def analyse_result_to_file(file2lack_dict, file2add_dict, file2modify_dict, file1, file2, file2lack, file2add, file2modify,save_statistical_res):
+def analyse_result_to_file(file2lack_dict, file2add_dict, file2modify_dict, file1, file2, file2lack, file2add,
+                           file2modify, save_statistical_res):
     result_str = ""
     result_str += "file1: " + file1 + ", file2: " + file2 + " 统计结果为：\n"
     result_str += "file2 缺失的行有: " + str(len(file2lack)) + "行\n"
@@ -132,13 +128,8 @@ def analyse_result_to_file(file2lack_dict, file2add_dict, file2modify_dict, file
         result_str += k + " " + v + "\n"
 
     try:
-        with open("./data/"+save_statistical_res, "w") as file:
+        with open("./data/" + save_statistical_res, "w") as file:
             file.write(result_str)
-        print("统计结果已成功保存到文件: "+save_statistical_res)
+        print("统计结果已成功保存到文件: " + save_statistical_res)
     except IOError:
         print("保存文件时出现错误，请检查路径和文件权限。")
-
-
-
-
-
--- tools/multisystem_performance/src/load_check.py
+++ tools/multisystem_performance/src/load_check.py
@@ -1,16 +1,17 @@
 from datetime import datetime
-import json
 import os
 import paramiko
 import getpass
 import logging
 import subprocess
 
-#生成时间戳，用于文件命名
+
+# 生成时间戳，用于文件命名
 def generate_timestamp_string():
     now = datetime.now()
     timestamp_string = now.strftime("%Y-%m-%d-%H-%M-%S")
     return timestamp_string
+
 
 timestamp = generate_timestamp_string()
 
@@ -21,6 +22,7 @@ logging.getLogger().addHandler(file_handler)
 logging.getLogger().setLevel(logging.INFO)
 logging.info('load_config.py start')
 
+
 # 测试pc1与pc2的连通性
 def connect_test(pc1, pc2):
     try:
@@ -29,7 +31,7 @@ def connect_test(pc1, pc2):
         # getpass.getpass() 用于隐藏输入的密码
 
         getpassword = getpass.getpass(f"请输入 {pc2['ip']} 的密码: ")
-        logging.info('请输入 %s 的密码:',{pc2['ip']} , extra={'logfile': log_file})
+        logging.info('请输入 %s 的密码:', {pc2['ip']}, extra={'logfile': log_file})
         # 默认端口号为22
         ssh.connect(hostname=pc2['ip'], port=22, username=pc2['user'], password=getpassword)
         logging.info('%s 与 %s 连通性测试成功', {pc1['ip']}, {pc2['ip']}, extra={'logfile': log_file})
@@ -47,19 +49,19 @@ def connect_test(pc1, pc2):
     finally:
         ssh.close()
 
-# host_test模式: 3台主机
-def host_test_body(data) :
 
+# host_test模式: 3台主机
+def host_test_body(data):
     local = {'ip': '', 'user': ''}  # 本机
-    pc1 = {'ip': '', 'user': ''}    # pc1
-    pc2 = {'ip': '', 'user': ''}    # pc2
-    
+    pc1 = {'ip': '', 'user': ''}  # pc1
+    pc2 = {'ip': '', 'user': ''}  # pc2
+
     if not data["host_test"]["local"]["ip"]:
         logging.info('本机IP缺失', extra={'logfile': log_file})
         local['ip'] = input("请输入本机IP, 或者直接编辑config.json文件: ")
     if not data["host_test"]["local"]["user"]:
         logging.info('本机用户名缺失', extra={'logfile': log_file})
-        local['user']= input("请输入本机用户名, 或者直接编辑config.json文件: ")
+        local['user'] = input("请输入本机用户名, 或者直接编辑config.json文件: ")
 
     if not data["host_test"]["pc1"]["ip"]:
         logging.info('pc1 IP缺失', extra={'logfile': log_file})
@@ -67,7 +69,7 @@ def host_test_body(data) :
     if not data["host_test"]["pc1"]["user"]:
         logging.info('pc1 用户名缺失', extra={'logfile': log_file})
         pc1['user'] = input("请输入pc1 用户名, 或者直接编辑config.json文件: ")
-    
+
     if not data["host_test"]["pc2"]["ip"]:
         logging.info('pc2 IP缺失', extra={'logfile': log_file})
         pc2['ip'] = input("请输入pc2 IP, 或者直接编辑config.json文件: ")
@@ -86,7 +88,7 @@ def host_test_body(data) :
         logging.info('本机与pc2连通性测试成功', extra={'logfile': log_file})
     except Exception as e:
         logging.info('本机与pc2连通性测试失败', extra={'logfile': log_file})
-        print("本机与pc2连通性测试失败")  
+        print("本机与pc2连通性测试失败")
     try:
         connect_test(pc1, pc2)
         logging.info('pc1与pc2连通性测试成功', extra={'logfile': log_file})
@@ -97,9 +99,9 @@ def host_test_body(data) :
         logging.info('无效的模式选择', extra={'logfile': log_file})
         print("无效的模式选择")
 
-# communication_test模式: 2台主机 
-def communication_test_body(data):
 
+# communication_test模式: 2台主机
+def communication_test_body(data):
     local = data['communication_test']['local']
     remote = data['communication_test']['remote']
     if not data["communication_test"]["local"]["ip"]:
@@ -107,7 +109,7 @@ def communication_test_body(data):
         local['ip'] = input("请输入本机IP, 或者直接编辑config.json文件: ")
     if not data["communication_test"]["local"]["user"]:
         logging.info('1 本机用户名缺失', extra={'logfile': log_file})
-        local['user']= input("请输入本机用户名, 或者直接编辑config.json文件: ")
+        local['user'] = input("请输入本机用户名, 或者直接编辑config.json文件: ")
     if not data["communication_test"]["remote"]["ip"]:
         logging.info('2 远程主机IP缺失', extra={'logfile': log_file})
         remote['ip'] = input("请输入远程主机IP, 或者直接编辑config.json文件: ")
@@ -120,7 +122,7 @@ def communication_test_body(data):
     print("远程主机用户名: ", remote['user'])
     connect_test(local, remote)
     logging.info('本机与远程主机连通性测试成功', extra={'logfile': log_file})
-    #except Exception as e:
+    # except Exception as e:
     #    logging.info('本机与远程主机连通性测试失败', extra={'logfile': log_file})
     #    print("本机与远程主机连通性测试失败")
 
@@ -128,23 +130,24 @@ def communication_test_body(data):
 def dependence_check():
     path = "./tools"
     print("检查: UnixBench 工具是否存在")
-    if os.path.exists(path+"/byte-unixbench//UnixBench"): 
-            print("检查: UnixBench 已存在,满足 CPU 与内存测试依赖")
+    if os.path.exists(path + "/byte-unixbench//UnixBench"):
+        print("检查: UnixBench 已存在,满足 CPU 与内存测试依赖")
     else:
         print("tools 目录下不存在 UnixBench,不满足 CPU 与内存测试依赖")
         if input("是否下载 UnixBench? (y/n)") == "y":
             try:
                 print("尝试执行命令: cd ./tools && git clone https://github.com/kdlucas/byte-unixbench.git ")
-                subprocess.run("cd ./tools && git clone https://github.com/kdlucas/byte-unixbench.git ", check = True)
+                subprocess.run("cd ./tools && git clone https://github.com/kdlucas/byte-unixbench.git ", check=True)
                 print("下载UnixBench成功")
             except:
-                print("下载UnixBench失败,请检查网络连接,或访问 https://github.com/kdlucas/byte-unixbench 手动克隆项目至 tools 目录")
+                print(
+                    "下载UnixBench失败,请检查网络连接,或访问 https://github.com/kdlucas/byte-unixbench 手动克隆项目至 tools 目录")
         else:
             print("请手动克隆 UnixBench 至 tools 目录")
             exit(1)
 
     print("检查: netperf 工具是否存在")
-    if os.path.exists(path+"/netperf"):
+    if os.path.exists(path + "/netperf"):
         print("检查: netperf 已存在,满足网络测试依赖")
     else:
         if subprocess.run("yum list installed | grep netperf", shell=True).returncode == 0:
@@ -154,14 +157,16 @@ def dependence_check():
             if input("是否下载 netperf? (y/n)") == "y":
                 try:
                     print("尝试执行命令: git clone https://github.com/HewlettPackard/netperf.git ./tools/")
-                    subprocess.run("/usr/bin/git clone https://github.com/HewlettPackard/netperf.git ./tools/", check = True)
-                    print("下载 netperf 成功")    
+                    subprocess.run("/usr/bin/git clone https://github.com/HewlettPackard/netperf.git ./tools/",
+                                   check=True)
+                    print("下载 netperf 成功")
                 except:
-                    print("下载 netperf 失败,请检查网络连接,或访问 https://github.com/HewlettPackard/netperf 手动克隆项目至 tools 目录")
+                    print(
+                        "下载 netperf 失败,请检查网络连接,或访问 https://github.com/HewlettPackard/netperf 手动克隆项目至 tools 目录")
             else:
                 print("请手动克隆 netperf 至 tools 目录")
                 exit(1)
-    
+
 
 def change_sysctl_parameters():
     if True:
@@ -171,4 +176,3 @@ def change_sysctl_parameters():
 def change_ulimit_parameters():
     if True:
         input("请按任意键继续...")
-
