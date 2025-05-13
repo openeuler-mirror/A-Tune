ff9e6f097d8f7c015461b8122681fecde804eb35
PshySimon <wodeqq2012@foxmail.com>
2025-05-13 07:18:50 +0000

    add benchmark for mysql
    Signed-off-by: PshySimon <wodeqq2012@foxmail.com>

---
+++ src/performance_benchmark/apply_mysql_params.py
@@ -0,0 +1,38 @@
+from src.utils.shell_execute import SshClient
+
+def apply_mysql_config(
+    ssh_client,
+    mysql_params: dict,
+    cnf_path="/etc/my.cnf"
+):
+    # 逐个检查并修改每个参数
+    for key, value in mysql_params.items():
+        # 使用 sed 命令检查是否已经有该参数
+        check_cmd = f"grep -q '^{key}\\s*=' {cnf_path}"
+        result = ssh_client.run_cmd(check_cmd)
+
+        if result.status_code == 0:
+            # 如果存在，则使用 sed 修改该参数的值
+            update_cmd = f"sed -i 's/^{key}\\s*=.*$/{key} = {value}/' {cnf_path}"
+            print(f"更新 {key} 参数为 {value}")
+        else:
+            # 如果不存在，则在 [mysqld] 部分添加该参数
+            add_cmd = f"sed -i '/\\[mysqld\\]/a {key} = {value}' {cnf_path}"
+            print(f"添加 {key} 参数为 {value}")
+        
+        # 执行更新或添加命令
+        ssh_client.run_cmd(update_cmd if result.status_code == 0 else add_cmd)
+
+    # 重启 mysqld 服务
+    print("🔄 已设置mysql参数，正在重启 mysqld ...")
+    result = ssh_client.run_cmd("systemctl restart mysqld")
+    exit_status = result.status_code
+
+    if exit_status == 0:
+        print("✅ mysqld 重启成功")
+    else:
+        error_msg = result.err_msg
+        print("❌ mysqld 重启失败")
+        print("错误信息：", error_msg)
+
+
---
+++ src/performance_benchmark/mysql_benchmark.py
@@ -0,0 +1,21 @@
+from typing import Dict, Any, List
+import logging
+import json
+import re
+
+from src.utils.shell_execute import cmd_pipeline, get_registered_cmd_funcs, SshClient
+from src.utils.thread_pool import ThreadPoolManager
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+
+@cmd_pipeline(cmd="cd /home/gitee/ && sh benchmark.sh")
+def parse_mysql_sysbench(output: str) -> Dict:
+    match = re.search(r"queries:\s+\d+\s+\(([\d.]+) per sec\.\)", output)
+
+    if match:
+        qps = match.group(1)
+        return {"qps": qps}
+    else:
+        raise RuntimeError("Failed to execute mysql benchmark")
+
