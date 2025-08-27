3e46d668eb90ad7854c4a93b3de6450078954e95
bingo <hubin73@huawei.com>
2025-08-27 10:01:45 +0000

    register global config format in __init__.py
    Signed-off-by: bingo <hubin73@huawei.com>

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,9 @@ from src.utils.common import display_banner
 
 logging.getLogger("paramiko.transport").propagate = False
 display_banner()
+
+logging.basicConfig(
+    level=logging.INFO,  # 设置日志级别
+    format="%(asctime)s - %(levelname)s [%(filename)s:%(funcName)s:%(lineno)d] - %(message)s",  # 设置日志格式
+    datefmt="%Y-%m-%d %H:%M:%S",  # 设置时间格式
+)
