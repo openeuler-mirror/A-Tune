88597a02756114ff86828680a391ad2728237596
wangyuhang <524413304@qq.com>
2025-09-12 15:43:48 +0800

    support non-standard json formats and return an empty value instead of raising an error for non-json strings.

--- src/utils/json_repair.py
+++ src/utils/json_repair.py
@@ -1,15 +1,24 @@
 import json
+import json5
+import logging
 from typing import Dict
 
 # TO implement more gernel repair: todo
 def json_repair(
     json_str: str
 ) -> Dict:
+    if not json_str or '{' not in json_str or '}' not in json_str:
+        logging.warning(f"the input is not a json string: {json_str}")
+        return {}
+    
     json_start = json_str.find('{')
     json_end = json_str.rfind('}') + 1
-    json_str = json_str[json_start:json_end]
-    try:
-        json_data = json.loads(json_str)
-    except json.decoder.JSONDecodeError as e:
-        raise RuntimeError(f"failed to parse json, raw json_str is {json_str}")
-    return json_data
+    json_candidate = json_str[json_start:json_end]
+    
+    for parser_name, parser in [("json.load", json.loads), ("json5.loads", json5.loads)]:
+        try:
+            return parser(json_candidate)
+        except Exception as e:
+            logging.warning(f"failed to parse json with {parser_name}\nstring: {json_str}\nreson：{e}")
+
+    return {}
