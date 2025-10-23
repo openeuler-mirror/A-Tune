12b96ea14efe5a0dcc93a5230b1bc530fb9a0add
jinsaihang <jinsaihang@h-partners.com>
2025-10-23 09:28:40 +0000

    modify some codes for oceanbase
    Signed-off-by: jinsaihang <jinsaihang@h-partners.com>

--- scripts/oceanbase/get_param.sh
+++ scripts/oceanbase/get_param.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# 功能：根据参数 scope 自动选择 observer / obproxy 设置参数
+# 功能：根据参数 scope 自动选择 observer / obproxy 查询参数
 # 用法：
-#   ./set_param.sh <param_name> <value>
+#   ./get_param.sh <param_name>
 
 # ===== 用户配置区 =====
 OBCLIENT_OBSERVER="obclient -hxxx -P2881 -uroot@sys -p'xxx' -Doceanbase -A"
@@ -9,13 +9,12 @@ OBCLIENT_PROXY="obclient -hxxx -P2883 -uroot@proxysys -p'xxx' -Doceanbase -A"
 PARAM_FILE="xxx/A-Tune-euler-copilot-tune/src/knowledge_base/knob_params/oceanbase.json"
 # =======================
 
-if [ $# -lt 2 ]; then
-    echo "用法：$0 <param_name> <value>"
+if [ $# -lt 1 ]; then
+    echo "用法：$0 <param_name>"
     exit 1
 fi
 
 PARAM=$1
-VALUE=$2
 
 # 校验参数文件
 if [ ! -f "$PARAM_FILE" ]; then
@@ -31,14 +30,14 @@ if [ "$SCOPE" == "null" ] || [ -z "$SCOPE" ]; then
     exit 1
 fi
 
-# ---------- 设置 ----------
+# ---------- 查询 ----------
 case "$SCOPE" in
   observer)
-    REMOTE_CMD="$OBCLIENT_OBSERVER -e \"ALTER SYSTEM SET $PARAM = $VALUE;\""
+    REMOTE_CMD="$OBCLIENT_OBSERVER -e \"SHOW PARAMETERS LIKE '$PARAM';\" | awk 'NR>3 && \$1==\"zone2\"{print \$7}'"
     ssh -q root@xxx "$REMOTE_CMD"
     ;;
   obproxy)
-    REMOTE_CMD="$OBCLIENT_PROXY -e \"ALTER PROXYCONFIG SET $PARAM = $VALUE;\""
+    REMOTE_CMD="$OBCLIENT_PROXY -e \"SHOW PROXYCONFIG LIKE '$PARAM';\" | awk -v name=\"$PARAM\" '\$1==name{print \$2}'"
     ssh -q root@xxx "$REMOTE_CMD"
     ;;
   *)
