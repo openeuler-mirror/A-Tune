ff3c7f0a8f5f9e1d3a70915b3baf17bf82ba4bf0
wangyuhang <524413304@qq.com>
2025-11-06 17:25:40 +0800

    provide an initial value for the log level

--- src/__init__.py
+++ src/__init__.py
@@ -5,8 +5,9 @@ from src.utils.common import display_banner
 logging.getLogger("paramiko.transport").propagate = False
 display_banner()
 
+level_name = str(config.__getitem__("log_level") or "INFO").upper()
 logging.basicConfig(
-    level=getattr(logging, config["log_level"].upper(), logging.INFO),
+    level=getattr(logging, level_name, logging.INFO),
     format="%(asctime)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
