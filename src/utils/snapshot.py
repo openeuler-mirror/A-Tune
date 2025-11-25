70dd3f72259ba95cfa8e7193b03e3f0e9e56b3fc
bingo <hubin73@huawei.com>
2025-11-25 07:04:32 +0000

    update snapshot.py
    Signed-off-by: bingo <hubin73@huawei.com>

--- src/utils/snapshot.py
+++ src/utils/snapshot.py
@@ -5,8 +5,8 @@ import logging
 import pickle
 from src.config import config
 
-save_snapshot = config["feature"][0]["save_snapshot"]
-use_snapshot = config["feature"][0]["use_snapshot"]
+enable_save_snapshot = config["feature"][0]["save_snapshot"]
+enable_use_snapshot = config["feature"][0]["use_snapshot"]
 snapshot_path_inited = False
 snapshot_path = ""
 
@@ -18,13 +18,13 @@ if not snapshot_path_inited:
         snapshot_path = os.path.join(os.getcwd(), "data", "snapshot")
         os.mkdirs(snapshot_path, mode=0o755, exist_ok=True)
     snapshot_path_inited = True
-    logging.info(f"save_snapshot: {save_snapshot}; use_snapshot: {use_snapshot}")
-    if save_snapshot or use_snapshot:
+    logging.info(f"save_snapshot: {enable_save_snapshot}; use_snapshot: {enable_use_snapshot}")
+    if enable_save_snapshot or enable_use_snapshot:
         logging.info(f"snapshot_path: {snapshot_path}")
 
 # 保存快照
-def save_snapshot(var, filename):
-    if not save_snapshot:
+def save_snapshot(var, filename: str):
+    if not enable_save_snapshot:
         return
     snapshot_file_path = os.path.join(snapshot_path, f"{filename}.pickle")
     with open(snapshot_file_path, 'wb') as f:
@@ -32,8 +32,8 @@ def save_snapshot(var, filename):
         logging.info(f"{filename} snapshot saved to {snapshot_file_path}")
 
 # 加载快照
-def load_snapshot(filename):
-    if not use_snapshot:
+def load_snapshot(filename: str):
+    if not enable_use_snapshot:
         return None
     try:
         snapshot_file_path = os.path.join(snapshot_path, f"{filename}.pickle")
